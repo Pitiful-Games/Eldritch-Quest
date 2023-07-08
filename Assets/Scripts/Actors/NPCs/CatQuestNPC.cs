71add49fb886e7ad738e9efa63080c1a797ca967
Jason Ngo <jngo102@aim.com>
2023-07-08 19:32:06 -0400

    Finished cat quest NPC

--- Assets/Scripts/Actors/NPCs/CatQuestNPC.cs
+++ Assets/Scripts/Actors/NPCs/CatQuestNPC.cs
@@ -1,9 +1,13 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
 public class CatQuestNPC : QuestNPC {
     protected override void CheckQuestComplete() {
         var inventory = UIManager.Instance.GetUI<Inventory>();
         if (inventory.GetItem(0) != null) {
             inventory.ClearItem(0);
             transform.Find("Cat").gameObject.SetActive(true);
+            GetComponent<Animator>().Play("Relieved");
             CompleteQuest();
         }
     }
